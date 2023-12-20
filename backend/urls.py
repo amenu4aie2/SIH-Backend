c91f56ed5dc4c49641f22e38fe39cc8edd397a7d
purush34 <purush3190@gmail.com>
2023-12-20 08:10:03 +0530

    go

--- backend/urls.py
+++ backend/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('doText/', views.doText, name='doText'),
     path('getEmp/', views.getEmp, name='getEmp'),
     path('getEmps/', views.getEmps, name='getEmps'),
+    path('replme/', views.replme, name='replme'),
     # Add more paths here
 ]
